8eabfafae8f76d77a308fd0a2afc6fc3fb8edd30
arfaat <arfatmaster8@gmail.com>
2026-06-30 01:41:22 +0530

    day 1 of learning python by projects

---
+++ ledger_provisioner.py
@@ -0,0 +1,9 @@
+account_id : int = 987654321
+legal_name :str = "Alpha Logistics Ltd"
+available_balance :float =15250.75
+is_active_merchant:bool = True
+print("---SYSTEM LEDGER INITIALIZATION--")
+print("Merchant ID:",account_id)
+print("Legal Entity:",legal_name)
+print("Current Balance USD:",available_balance)
+print("System Status Active:",is_active_merchant)
